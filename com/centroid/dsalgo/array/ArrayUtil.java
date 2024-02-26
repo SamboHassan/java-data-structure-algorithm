c40e38053d718e53b5ef8acf3627e24a92e027e8
Hassan Sambo <sambohasan@gmail.com>
2024-02-26 11:03:20 +0100

    add missing number algorithm

--- com/centroid/dsalgo/array/ArrayUtil.java
+++ com/centroid/dsalgo/array/ArrayUtil.java
@@ -1,4 +1,40 @@
 package com.centroid.dsalgo.array;
 
 public class ArrayUtil {
+    public void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public int[] resize(int[] arr, int capacity){
+        int[] temp = new int[capacity];
+        for (int i=0; i<arr.length; i++){
+            temp[i] = arr[i];
+        }
+        return temp;
+    }
+
+    public static int findMissingNumber(int[] arr){
+        int n = arr.length + 1;
+        int sum = n * (n + 1) / 2;
+        for (int num : arr){
+            sum = sum - num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ArrayUtil arrayUtil = new ArrayUtil();
+//        arrayUtil.printArray(new int[] {5,1,2,9,10});
+        int[] original = new int[] {5,1,2,9,10};
+        System.out.println("The size of the array is: " + original.length);
+        original = arrayUtil.resize(original, 10);
+        System.out.println("The size of the array is: " + original.length);
+        int[] arr = new int[]{1,3,6,8,2,4,7};
+        System.out.println(findMissingNumber(arr));
+
+    }
 }
--- com/centroid/dsalgo/array/MoveZeros.java
+++ com/centroid/dsalgo/array/MoveZeros.java
@@ -15,22 +15,39 @@ public class MoveZeros {
         moveZeros(arr, arr.length);
         printArray(arr);
     }
-    public void moveZeros(int[] arr, int n){
-       int j = 0; //j pointer will keep track of the zeroth element
-       for (int i=0; i<n; i++){
-           if (arr[i] != 0 && arr[j] == 0){ // i will focus on non zero elements
-               int temp = arr[i];
-               arr[i] = arr[j];
-               arr[j] = temp;
-           }
-           // Since j will only focus on zeroth element, we move on to next element
-           // if arr[j] is not zero
-           // Otherwise we stay to do a swap
-           if (arr[j] != 0){
-               j++;
-           }
-       }
-    }
+
+  public void moveZeros(int[] arr, int n){
+        int j = 0;
+        for (int i=0; i<n; i++){
+            if (arr[j] != 0){
+                j++;
+            }
+            if (arr[i] != 0 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+  }
+
+
+
+//    public void moveZeros(int[] arr, int n){
+//       int j = 0; //j pointer will keep track of the zeroth element
+//       for (int i=0; i<n; i++){
+//           if (arr[i] != 0 && arr[j] == 0){ // i will focus on non zero elements
+//               int temp = arr[i];
+//               arr[i] = arr[j];
+//               arr[j] = temp;
+//           }
+//           // Since j will only focus on zeroth element, we move on to next element
+//           // if arr[j] is not zero
+//           // Otherwise we stay to do a swap
+//           if (arr[j] != 0){
+//               j++;
+//           }
+//       }
+//    }
 
     public static void main(String[] args) {
         MoveZeros arrUtil = new MoveZeros();
